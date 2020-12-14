67f31e29145eb2d3f6d842ae4fb6bca00bdc7d2b
animesharma3 <animesharma3@gmail.com>
2020-12-14 10:25:36 +0530

    'test'

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -18,7 +18,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyResume.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
